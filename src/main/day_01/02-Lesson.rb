67b62c4bf9e6c608c95e1f4dd3014f1e53207188
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-29 22:59:38 -0400

    print and puts

---
+++ src/main/day_01/02-Lesson.rb
@@ -0,0 +1,14 @@
+class SecondLesson
+  #use variable
+  #Boolean = True of False
+  #Integer = Number
+  #Float = number whith decimal 4.567
+  #String = Anaything in quotations
+
+  my_cat = "TimTim"
+  age = 2
+
+  #print variable
+  print "May cat have name: " + my_cat + "\n"
+  print age
+end
