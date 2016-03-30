04d41db644dda4ae55806e7edd35f839384e5061
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-29 23:19:18 -0400

    Conditional if, else and elsif

--- src/main/day_03/05-Conditional.rb
+++ src/main/day_03/05-Conditional.rb
@@ -1,3 +1,15 @@
 class Conditional
+  you_age = 21
 
+  if you_age <= 17
+    puts "You are not old enough for this site!"
+
+  elsif you_age > 100
+    puts "Go to this site: http://globoesporte.globo.com"
+
+  elsif you_age == 21
+    puts "You just got updated to the Premium Version"
+  else
+    puts "What is your gender?"
+  end
 end
