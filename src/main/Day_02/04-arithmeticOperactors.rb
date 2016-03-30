43c921c0828a9a3e47e3c9ddd8960e16046d5248
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-29 22:51:38 -0400

    Arithmetic Operactors, while and if

---
+++ src/main/Day_02/04-arithmeticOperactors.rb
@@ -0,0 +1,19 @@
+class ArithmeticOperactors
+  #Addition
+  puts 6 + 15
+
+  #Subtraction
+  puts 20 - 10
+
+  #Multiplication
+  puts 8 * 10
+
+  #Division
+  puts 100 / 10
+
+  #Exponential
+  puts 10 ** 4
+
+  #Percentage
+  puts 17 % 4
+end
