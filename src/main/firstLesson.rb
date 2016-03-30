0011aa78dda8d92119dc3ea20bd7880b0fd9afa7
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-29 22:10:33 -0400

    achar números primos entre dois numeros

--- src/main/firstLesson.rb
+++
@@ -1,9 +0,0 @@
-class FirstLesson
-  #print value
-  print "This is firts line in ruby. "
-  puts "This is my second line."
-  print "This is my gun. "
-  puts "My gun is my only friend"
-  puts "I need more friends"
-  print "let's stop talking about this\n"
-end
