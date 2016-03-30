7d913f470682fbaa8c4cfa17fbba384cdd68fd71
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-29 22:50:07 -0400

    print value

---
+++ src/main/Day_01/01-Lesson.rb
@@ -0,0 +1,9 @@
+class FirstLesson
+  #print value
+  print "This is firts line in ruby. "
+  puts "This is my second line."
+  print "This is my gun. "
+  puts "My gun is my only friend"
+  puts "I need more friends"
+  print "let's stop talking about this\n"
+end
