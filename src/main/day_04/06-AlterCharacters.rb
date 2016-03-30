19c54e62ad3265581b15bd39c9732c3caa57f059
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-29 23:46:55 -0400

    read some characters in the text

--- src/main/day_04/06-AlterCharacters.rb
+++ src/main/day_04/06-AlterCharacters.rb
@@ -1,4 +1,4 @@
-class Characters
+class AlterCharacters
   my_mame = "Anselmo Pfeifer"
 
   puts my_mame.capitalize
@@ -6,4 +6,5 @@ class Characters
   puts my_mame.downcase
   puts my_mame.reverse
   puts my_mame.length
+
 end
