e685eb6a7bc5ac9e13368e48377f93357ce816ff
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-03-30 22:30:36 -0400

    Dia 05 Array

--- src/main/day_05/Array.rb
+++ src/main/day_05/Array.rb
@@ -1,4 +1,14 @@
 class Array
   rock_booton = "Mary kate Olson Lindsay Lohan Charlie Sheen"
   puts rock_booton
+
+  date = "30/03/2016"
+  puts date.split("/")
+  puts date[2]
+  puts date[0..4]
+  puts date[0...5]
+
+  array_families = ["Braday", "Partridge", "Bunday"]
+
+  puts array_families
 end
