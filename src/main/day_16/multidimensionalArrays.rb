74ddf477f60ccd6bd196a5e4b3f2f08227117fb8
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-05-29 01:43:13 -0400

    Coletando dados de um array.

---
+++ src/main/day_16/multidimensionalArrays.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+number_array = [100,200,300,400,500,600,700,800,900,1000]
+number_arrays = [number_array, number_array, number_array]
+string_arrays = [["Limao", "Mamao", "Uva"],["Uva", "Limão", "Laranja"],["Laranja", "Uva", "Limao"]]
+
+
+puts "O tamanho do number_array é : #{number_array.size}"
+puts "O tamanho da string_array é : #{string_arrays.size}"
+
+puts "O resultado do segundo numero do ultimo array é : #{number_arrays[2][2] * 5}"
+
+puts "A utima letra da ultima palavra do ultimo array eh: #{string_arrays[2][string_arrays.size-1][4]}"
