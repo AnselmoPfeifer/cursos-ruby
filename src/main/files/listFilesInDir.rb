7587942d81b26274774c68261f860ba88df673f0
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-05-29 23:20:46 -0400

    Lista arquivos e imprime nome e tamanho deles.

--- src/main/files/listFilesInDir.rb
+++ src/main/files/listFilesInDir.rb
@@ -1,21 +1,12 @@
 #!/usr/bin/env ruby
 
-# Use este script para buscar aquivos de uma determinada extenção
-# Exemplo buscar em /home/usuario/ por arquivos zip, tar, tar.gz
-
-puts "Lista arquivos de um diretorio e imprime diretorio/arquivo e \n tamanho em Mb"
 diretorio = "/Users/anselmo/Documents/Desenvolvimento/workspace/"
-
 extensoes = "{zip,tar,tar.gz}"
 buscar_por = "#{diretorio}**/*.#{extensoes}"
 
 Dir.glob(buscar_por).each do |arquivos|
   puts "#{arquivos}"
-  tamanho = (File.size(arquivos).to_f / 2**20).round(2)
-
+  tamanho = (File.size(arquivos).to_f / 2**20).round(2).floor
   puts "#{tamanho} Mb"
 end
 
-# A impressao sera algo assim
-# /Users/anselmo/Documents/Desenvolvimento/workspace/java/jdk-8u65-linux-x64.tar.gz
-# 172.86 Mb
