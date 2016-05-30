c3ba17685176b2a05a5789b4437e7ba130931886
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-05-30 13:50:34 -0400

    Lista arquivos e imprime nome e tamanho deles.

--- src/main/files/listFilesInDir.rb
+++ src/main/files/listFilesInDir.rb
@@ -1,12 +1,33 @@
 #!/usr/bin/env ruby
 
-diretorio = "/Users/anselmo/Documents/Desenvolvimento/workspace/"
+diretorio = "/Users/anselmo/Documents/Desenvolvimento/workspace/Servidor/Java/"
 extensoes = "{zip,tar,tar.gz}"
 buscar_por = "#{diretorio}**/*.#{extensoes}"
 
-Dir.glob(buscar_por).each do |arquivos|
+Dir.glob(buscar_por).sort!{|arquivos| File.size(arquivos)}.each do |arquivos|
   puts "#{arquivos}"
   tamanho = (File.size(arquivos).to_f / 2**20).round(2).floor
   puts "#{tamanho} Mb"
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Dir.glob(buscar_por).each do |arquivos|
+#   puts "#{arquivos}"
+#   tamanho = (File.size(arquivos).to_f / 2**20).round(2).floor
+#   puts "#{tamanho} Mb"
+# end
+
 
