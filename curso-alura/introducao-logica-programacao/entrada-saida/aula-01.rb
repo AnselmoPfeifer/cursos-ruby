ee9820442837b91657717e523fbbae35e423b331
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-07 23:52:43 -0400

    Adicionando aula 01 e 02

--- curso-alura/introducao-logica-programacao/entrada-saida/aula-01.rb
+++ curso-alura/introducao-logica-programacao/entrada-saida/aula-01.rb
@@ -1,17 +1,84 @@
 #!/usr/bin/env ruby
-puts "Bem vindo ao curso de Adivinhação"
-puts "Qual é seu nome?"
+puts "Bem vindo ao curso de Adivinhação\n
+Qual é seu nome?"
+
 nome = gets
+
 puts "Começaremos o jogo com você " + nome
 
 puts "Escolhendo um numero secreto entre 0 e 200"
 numero_secreto = 175
-puts "Escolhido... Que tal adivinhar hoje o numero secreto?"
-puts "Entre com seu numero:"
-puts "Tentativa 1:"
+
+puts "Tentativa 1"
 chute = gets
 puts "Será que acertou, Você chutou " + chute
 
-#Converte String em numero Inteiro
 puts chute.to_i == numero_secreto
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    nome + "Você acertou. Parabens!"
+else
+  puts "Você errou, Tente novamente!"
+
+    maior = numero_secreto > chute.to_i
+  if maior
+    puts "O numero secreto é maior!"
+  else
+    puts "O numero secreto é menor!"
+  end
+end
+
+puts "Tentativa 2"
+chute = gets
+puts "Será que acertou, Você chutou " + chute
+
+puts "Converte String em numero Inteiro"
+puts chute.to_i == numero_secreto
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+  nome + "Você acertou. Parabens!"
+else
+  puts "Você errou, Tente novamente!"
+
+  maior = numero_secreto > chute.to_i
+  if maior
+    puts "O numero secreto é maior!"
+  else
+    puts "O numero secreto é menor!"
+  end
+end
+
+puts "Tentativa 3"
+chute = gets
+puts "Será que acertou, Você chutou " + chute
+
+puts "Converte String em numero Inteiro"
+puts chute.to_i == numero_secreto
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+  nome + "Você acertou. Parabens!"
+else
+  puts "Você errou, Tente novamente!"
+
+  maior = numero_secreto > chute.to_i
+  if maior
+    puts "O numero secreto é maior!"
+  else
+    puts "O numero secreto é menor!"
+  end
+end
+
+#Operadores matematicos
+# puts 175 > 174
+# puts 174 < 175
+# puts 170 >= 170
+# puts 175 >= 170
+# puts 170 <= 170
+# puts 170 <= 175
 
---
+++ curso-alura/introducao-logica-programacao/entrada-saida/aula-02.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+puts "Bem vindo ao curso de Adivinhação\n
+Qual é seu nome?"
+
+nome = gets
+
+puts "Começaremos o jogo com você " + nome
+
+puts "Escolhendo um numero secreto entre 0 e 200"
+numero_secreto = 175
+
+for contagem in 1..3
+  puts "teste " + contagem.to_s
+end
+
+
+
