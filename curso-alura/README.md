f8b9ac1f84fd5aefac967fefdf8dcd0ea960c5c4
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-09 17:53:35 -0400

    Trabalhando com condicoes e lacos no ruby

---
+++ curso-alura/README.md
@@ -0,0 +1,1 @@
+# Introducao a programacao com ruby e jogos
--- curso-alura/introducao-logica-programacao/2-condicoes-lacos/aula-02.rb
+++ curso-alura/introducao-logica-programacao/2-condicoes-lacos/aula-02.rb
@@ -8,9 +8,29 @@ puts "Começaremos o jogo com você " + nome
 
 puts "Escolhendo um numero secreto entre 0 e 200"
 numero_secreto = 175
+numero_tentativas = 2
+for contagem in 1..numero_tentativas
+  puts "Tentativa " + contagem.to_s + " de " + numero_tentativas.to_s
+  chute = gets
+  puts "Será que acertou, Você chutou " + chute
 
-for contagem in 1..3
-  puts "teste " + contagem.to_s
+  puts chute.to_i == numero_secreto
+
+  acertou = numero_secreto == chute.to_i
+
+  if acertou
+    puts nome + " Você acertou. Parabens!"
+    break
+  else
+    puts nome +  "Você errou, Tente novamente!"
+
+    maior = numero_secreto > chute.to_i
+    if maior
+      puts "O numero secreto é maior!"
+    else
+      puts "O numero secreto é menor!"
+    end
+  end
 end
 
 
