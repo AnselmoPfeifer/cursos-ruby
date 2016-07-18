f24eed5835d45c116e4ec8af6ed29c668c3e0ba8
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-18 14:53:00 -0400

    Iniciando a aula 05 de Operadores matematicas, Usando todos os operadores matematicos

---
+++ curso-alura/introducao-logica-programacao/test/aula-05/resto_divisao.rb
@@ -0,0 +1,18 @@
+primeiro_numero = 13
+segundo_numero = 1
+
+puts primeiro_numero / segundo_numero
+puts primeiro_numero % segundo_numero #Resto da divisao 13 / 5 = 3
+puts primeiro_numero + segundo_numero
+puts primeiro_numero - segundo_numero
+puts primeiro_numero * segundo_numero
+
+numero = 5
+numero += 1 #6
+numero -= 2 #4
+numero *= 2 #8
+numero /= 4 #2
+numero *= 13 #26
+numero %= 10 #6
+
+puts numero
