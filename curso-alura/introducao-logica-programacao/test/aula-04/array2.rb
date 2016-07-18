e790ef09f863088fb602a9f409bfe275d84ae57f
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-17 23:29:19 -0400

    Iniciando a aula 04 de Arrays, imprimindo todos os metodos de um valor

--- curso-alura/introducao-logica-programacao/test/aula-04/array2.rb
+++ curso-alura/introducao-logica-programacao/test/aula-04/array2.rb
@@ -9,7 +9,7 @@ tentativa = 2
 chutes[tentativa -1] = chute
 
 for contador in 0..(tentativa -1)
-  puts "Chute " + chutes[contador].to_s
+  puts "Agora sim. Nosso código roda e funciona, imprimindo os números que chutamos até agora: " + chutes[contador].to_s
 end
 
 puts chutes
