7326aa6af073bd80fd60b4d9ae103b37e6155503
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-18 14:16:54 -0400

    Iniciando a aula 05 de Operadores matematicas, criando exemplos de uso do caso unless

---
+++ curso-alura/introducao-logica-programacao/test/aula-05/unless.rb
@@ -0,0 +1,9 @@
+pontos_a_perder = -100
+puts pontos_a_perder
+
+#caso o pontos_a_perder seja maior que 0 ignore senao inverte o sinal
+unless pontos_a_perder > 0
+  pontos_a_perder *= -1
+end
+
+puts pontos_a_perder
