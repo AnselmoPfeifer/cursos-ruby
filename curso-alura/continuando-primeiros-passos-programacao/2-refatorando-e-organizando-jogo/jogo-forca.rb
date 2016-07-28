36388955d3c05f0eb2f8c345fa5f15eacbbb1736
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-28 17:19:31 -0400

    Extraindo partes principal do codigo para main.rb

--- curso-alura/continuando-primeiros-passos-programacao/2-refatorando-e-organizando-jogo/jogo-forca.rb
+++ curso-alura/continuando-primeiros-passos-programacao/2-refatorando-e-organizando-jogo/jogo-forca.rb
@@ -46,9 +46,12 @@ def joga(nome)
   avisa_pontos(pontos_ate_agora)
   end
 
-nome = dar_boas_vindas
+def jogo_da_forca
+  nome = dar_boas_vindas
 
-loop do
-  joga nome
-  break if nao_quer_jogar?
+  loop do
+    joga nome
+    break if nao_quer_jogar?
+  end
+
 end
---
+++ curso-alura/continuando-primeiros-passos-programacao/2-refatorando-e-organizando-jogo/main.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require_relative 'jogo-forca'
+
+jogo_da_forca
