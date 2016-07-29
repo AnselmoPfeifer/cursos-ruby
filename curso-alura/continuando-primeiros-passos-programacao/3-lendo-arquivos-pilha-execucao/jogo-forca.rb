8d45772e4df2813c5da919b5209fa3adfb050105
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-28 22:45:09 -0400

    Inciando aula 03 com leitura de Arquivos

---
+++ curso-alura/continuando-primeiros-passos-programacao/3-lendo-arquivos-pilha-execucao/jogo-forca.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+require_relative 'ui'
+
+def sorteia_palavra_secreta
+  avisa_escolhendo_palavra
+  texto = File.read("dicionario2.txt")
+  todas_as_palavras = texto.split("\n")
+  numero_escolhido = rand(todas_as_palavras.size)
+  palavra_secreta = todas_as_palavras[numero_escolhido].downcase
+  avisa_palavra_escolhida palavra_secreta
+end
+
+def palavra_mascarada(chutes, palavra_secreta)
+  mascara = ""
+  for letra in palavra_secreta.chars
+    if chutes.include? letra
+      mascara << letra
+    else
+      mascara << "_"
+    end
+  end
+  mascara
+end
+
+def pede_um_chute_valido(chutes, erros, mascara)
+  cabecalho_tentativa chutes, erros, mascara
+  loop do
+    chute = pede_um_chute
+    if chutes.include? chute
+      avisa_chute_efetuado chute
+    else
+      return chute
+    end
+  end
+end
+
+def joga(nome)
+  palavra_secreta = sorteia_palavra_secreta
+  erros = 0
+  chutes = []
+  pontos_ate_agora = 0
+
+  while erros < 5
+    mascara = palavra_mascarada chutes, palavra_secreta
+    chute = pede_um_chute_valido chutes, erros, mascara
+
+    chutes << chute
+
+    chutou_uma_letra = chute.size == 1
+
+    if chutou_uma_letra
+      letra_procurada = chute[0]
+
+      total_encrontrado = palavra_secreta.count letra_procurada
+      if total_encrontrado == 0
+        avisa_letra_nao_encontrada
+        erros += 1
+      else
+        avisa_letra_encontrada total_encrontrado
+      end
+    else
+      acertou = chute == palavra_secreta
+      if acertou
+        avisa_acertou_palavra
+        pontos_ate_agora += 100
+        break
+      else
+        avisa_errou_palavra
+        pontos_ate_agora -=30
+        erros +=1
+
+      end
+    end
+  end
+
+  avisa_pontos(pontos_ate_agora)
+  end
+
+def jogo_da_forca
+  nome = dar_boas_vindas
+
+  loop do
+    joga nome
+    break if nao_quer_jogar?
+  end
+
+end
---
+++ curso-alura/continuando-primeiros-passos-programacao/3-lendo-arquivos-pilha-execucao/ui.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+def avisa_chute_efetuado (chute)
+  puts "Você ja chutou '#{chute}'"
+end
+
+def avisa_letra_nao_encontrada
+  puts "Letra nao encontrada."
+end
+
+def avisa_letra_encontrada (total_encrontrado)
+  puts "Letra encontrada #{total_encrontrado} vezes."
+end
+
+def avisa_acertou_palavra
+  puts "Parabens! Você Acertou a palavra!"
+end
+
+def avisa_errou_palavra
+  puts "Que pena você errou!"
+end
+
+def avisa_pontos(pontos_ate_agora)
+  puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def dar_boas_vindas
+  puts
+  puts "        P  /_\  P                              "
+  puts "       /_\_|_|_/_\                             "
+  puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+  puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+  puts "  |' '  |  |_|  |'  ' |                        "
+  puts "  |_____| ' _ ' |_____|                        "
+  puts "        \__|_|__/                              "
+  puts
+  puts "Qual é seu nome?"
+  nome = gets.strip
+  puts "Começaremos o jogo com você #{nome}"
+  nome
+end
+
+def avisa_escolhendo_palavra
+  puts "Escolhendo uma palavra...Aguarde..."
+end
+
+def avisa_palavra_escolhida (palavra_secreta)
+  puts "Escolhida uma palavra com #{palavra_secreta.size} letras... boa sorte!"
+  palavra_secreta
+end
+
+def nao_quer_jogar?
+  puts "Deseja jogar de novo? (S/N)"
+  quero_jogar = gets.strip
+  nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+def cabecalho_tentativa(chutes, erros, mascara)
+  puts "\n\n"
+  puts "palavra secreta eh a #{mascara}"
+  puts "Erros ate agora: #{erros}"
+  puts "Chutes ate agora: #{chutes}"
+  puts "Entre com uma palavra ou letra"
+end
+
+def pede_um_chute
+  chute = gets.strip
+  puts "Será que acertou? você chutou #{chute}"
+  chute
+end
