34b44c9456404ecffef4a86147cdaceedda1da60
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-30 17:20:14 -0400

    Escrevendo duas funcoes que visa encontrar o heroi

--- curso-alura/mais-logica-programacao-novo-jogo/1-lidando-com-arrays/foge-forge.rb
+++ curso-alura/mais-logica-programacao-novo-jogo/1-lidando-com-arrays/foge-forge.rb
@@ -6,6 +6,31 @@ def le_mapa(numero)
   mapa = texto.split("\n")
 end
 
+#Primeira forma de encontrar o Heroi
+# def encontra_jogador(mapa)
+#   caracter_do_heroi = "H"
+#   for linha = 0..(mapa.size-1)
+#   linha_atual = mapa[linha]
+#     coluna_do_heroi = linha_atual.index caracter_do_heroi
+#     if coluna_do_heroi
+#       # achei!
+#     end
+#   end
+#   # não achei!
+# end
+
+#Segunda forma de encontrar o Heroi
+def encontra_jogador(mapa)
+  caracter_do_heroi = "H"
+  mapa.each_with_index do | linha_atual, linha |
+    coluna_do_heroi = linha_atual.index caracter_do_heroi
+    if coluna_do_heroi
+      # achei!
+    end
+  end
+  # não achei!
+end
+
 def joga(nome)
   mapa = le_mapa (1)
 
