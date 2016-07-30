a73f3dd99b262b2495685fa39db8f072999a0c34
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-07-30 08:58:59 -0400

    Criando mapas para o jogo de foge foge

--- curso-alura/mais-logica-programacao-novo-jogo/1-lidando-com-arrays/ui.rb
+++ curso-alura/mais-logica-programacao-novo-jogo/1-lidando-com-arrays/ui.rb
@@ -8,3 +8,12 @@ def dar_boas_vindas
   puts "Começaremos o jogo com você #{nome}"
   nome
 end
+
+def desenha(mapa)
+  puts mapa
+end
+
+def pede_movimento
+  puts "Para onde deseja ir?"
+  movimento = gets.strip
+end
