dd9bc321a20bcc952123834344199d162d0a44d0
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-13 14:24:25 -0300

    Fizalizando aula 04 (enxugando seu codigo)

--- orientacao-objetos/4-enxugando-codigo/sistema.rb
+++ orientacao-objetos/4-enxugando-codigo/sistema.rb
@@ -1,14 +1,20 @@
 require_relative 'livro'
 require_relative 'estoque'
 
-algoritmos = Livro.new("Programacao O O em Ruby", 70, 2000, true, "")
-livro_arquitetura_software = Livro.new("Arquitetura de software", 70, 2000, true, "")
+algoritmos = Livro.new('Programacao O O em Ruby', 70, 2000, true, 'Globo', 'livro')
+livro_arquitetura_software = Livro.new('Arquitetura de software', 70, 2000, true, 'Globo2', 'livro')
+revista = Livro.new('Revista de Ruby', 10, 2012, true, 'Globo', 'revista')
 
 estoque = Estoque.new
 estoque <<  algoritmos << algoritmos
-estoque <<  livro_arquitetura_software << livro_arquitetura_software
+estoque << livro_arquitetura_software << livro_arquitetura_software
+estoque << revista << revista << revista
 
 estoque.vende algoritmos
 estoque.vende algoritmos
+estoque.vende livro_arquitetura_software
+estoque.vende revista
+estoque.vende revista
 
 puts estoque.livro_que_mais_vendeu_por_titulo.titulo
+puts estoque.revista_que_mais_vendeu_por_titulo.titulo
